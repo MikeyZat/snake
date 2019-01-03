666a24a26a4feb8ab4dde073947cabfd061c04f4
MikeyZat <mikeyzat@gmail.com>
2019-01-03 13:36:27 +0100

    Updates

---
+++ precache-manifest.7cf73575dbbf557043be575fbfd51c75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48b6b087ccaf43eb9d3a",
+    "url": "/snake/static/css/main.3b0e4427.chunk.css"
+  },
+  {
+    "revision": "48b6b087ccaf43eb9d3a",
+    "url": "/snake/static/js/main.48b6b087.chunk.js"
+  },
+  {
+    "revision": "6bd1ea98e814ca143deb",
+    "url": "/snake/static/js/1.6bd1ea98.chunk.js"
+  },
+  {
+    "revision": "da1ecade61dd97107e27",
+    "url": "/snake/static/js/runtime~main.da1ecade.js"
+  },
+  {
+    "revision": "ad16bf82a2b67ca01c5d57e132f63b9c",
+    "url": "/snake/index.html"
+  }
+];
